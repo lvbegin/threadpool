3129897a6d0283109f6346f4bc60306e7aca2e06
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-04-01 21:27:42 +0200

    INT: cleanup

--- test/threadpoolTest.cpp
+++ test/threadpoolTest.cpp
@@ -1,89 +1,77 @@
 #include <iostream>
 #include <atomic>
-
-/*
- * 1. remove context
- * 2. improve tests by add test on counters.
- */
+#include <assert.h>
 
 #include <threadpool.h>
 #include <threadCache.h>
 
 using namespace threadpool;
 
-class TestThreadBody__no_thread_context {
-
-public:
-	TestThreadBody__no_thread_context() = default;
-	~TestThreadBody__no_thread_context() = default;
-
-	void operator() (std::string &message)
-	{
-		if (message == reference_message)
-			nbCalls++;
-		return ;
-	}
-	static std::string reference_message;
-	static std::atomic<int> nbCalls;
-};
-
-std::atomic<int> TestThreadBody__no_thread_context::nbCalls {0};
-std::string TestThreadBody__no_thread_context::reference_message {"Hello world!"};
-
-static std::atomic<unsigned int> initDone {0};
-static std::atomic<unsigned int> finalDone {0};
-
-auto init = []() { initDone++; };
-auto final = []() { finalDone++; };
-
 static void executeThreadPool__no_thread_context(int nbMessages)
 {
 
-	TestThreadBody__no_thread_context body;
-	Threadpool<std::string> t(doNothing, body, doNothing, 5, nbMessages);
+	std::cout << "Execute threadpool without thread context: ";
+
+	static const std::string message("Hello World");
+	std::atomic<int> messageReceived {0};
+	auto  t = std::make_unique<Threadpool<const std::string>>(doNothing, [&messageReceived](const std::string &m) {assert (m == message); messageReceived++;}, doNothing, 5, nbMessages);
 	for (int i = 0; i < nbMessages; i++)
-		t.add(TestThreadBody__no_thread_context::reference_message);
+		t->add(message);
+
+	t.reset(nullptr);
+	if (messageReceived == nbMessages)
+		std::cout << "OK" << std::endl;
+	else
+		std::cout << "NOK" << std::endl;
 }
 
-static void executeThreadPool__init_and_final_but_no_thread_context(int nbMessages)
+static void executeThreadPool__init_and_final(int nbMessages)
 {
+	std::cout << "Execute threadpool with init and final functions but without thread context: ";
+	static const std::string message("Hello world");
+	static std::atomic<unsigned int> initDone {0};
+	static std::atomic<unsigned int> finalDone {0};
 
-	TestThreadBody__no_thread_context body;
-	Threadpool<std::string> t( init, body, final, 5, nbMessages);
+	auto init = []() { initDone++; };
+	auto final = []() { finalDone++; };
+
+	auto  t = std::make_unique<Threadpool<const std::string>>(init, [](const std::string &) {}, final, 5, nbMessages);
 	for (int i = 0; i < nbMessages; i++)
-		t.add(TestThreadBody__no_thread_context::reference_message);
+		t->add(message);
+
+	t.reset(nullptr);
+	if (5 == initDone.load() && 5 == finalDone.load())
+		std::cout << "OK" << std::endl;
+	else
+		std::cout << "NOK" << std::endl;
+
 
 }
 
-static void Cache__basic(int nbMessages)
+static void executeThreadPool__use_external_thread_cache(int nbMessages)
 {
-	TestThreadBody__no_thread_context body;
-	ThreadCache cache(2);
-	Threadpool<std::string> t(init, body, final, 1, nbMessages, cache);
+	std::cout << "Execute threadpool with  external thread cache: ";
+
+	static const std::string message("Hello World");
+	std::atomic<int> messageReceived {0};
+	auto cache = std::make_unique<ThreadCache>(2);
+
+	auto  t = std::make_unique<Threadpool<const std::string>>(doNothing, [&messageReceived](const std::string &m) {assert (m == message); messageReceived++;}, doNothing, 1, nbMessages, *cache.get());
 	for (int i = 0; i < nbMessages; i++)
-		t.add(TestThreadBody__no_thread_context::reference_message);
+		t->add(message);
+
+	t.reset(nullptr);
+	cache.reset(nullptr);
+	if (messageReceived == nbMessages)
+		std::cout << "OK" << std::endl;
+	else
+		std::cout << "NOK" << std::endl;
 }
 
 int main()
 {
 	static const int nbMessages = 100000;
 	executeThreadPool__no_thread_context(nbMessages);
-	std::cout << "Execute threadpool without thread context: ";
-	if (TestThreadBody__no_thread_context::nbCalls.load() == nbMessages)
-		std::cout << "OK" << std::endl;
-	else
-		std::cout << "NOK" << std::endl;
-
-	executeThreadPool__init_and_final_but_no_thread_context(nbMessages);
-	std::cout << "Execute threadpool with init and final functions but without thread context: ";
-	if (5 == initDone.load() && 5 == finalDone.load())
-		std::cout << "OK" << std::endl;
-	else
-		std::cout << "NOK" << std::endl;
-	Cache__basic(nbMessages);
-	if (TestThreadBody__no_thread_context::nbCalls.load() == (3 * nbMessages))
-		std::cout << "OK" << std::endl;
-	else
-		std::cout << "NOK" << std::endl;
-
+	executeThreadPool__init_and_final(nbMessages);
+	executeThreadPool__use_external_thread_cache(nbMessages);
 }
