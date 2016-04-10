122e79c25bcffd0d9de5541cf691715ecca36dc1
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-04-10 19:14:19 +0200

    INT: reduce the size of the queue.

--- include/map.h
+++ include/map.h
@@ -37,7 +37,7 @@ using namespace threadpool;
 
 template<typename M>
 void map(std::vector<M> &v, std::function<void( M&)> f, ThreadCache &cache) {
-	Threadpool<M> pool(doNothing, f, doNothing, 10, v.size(), cache);
+	Threadpool<M> pool(doNothing, f, doNothing, 10, 10, cache);
 	std::for_each(v.begin(), v.end(), [&pool](M &e) { pool.add(e); });
 }
 
